85e1dfd9f2d5fe46bd4b31741a01d78ac0d904b6
yoonison722 <yoonison722@gomcorp.com>
2021-06-04 18:10:07 +0900

    피드백 정리 - 1

--- src/js/ui.js
+++ src/js/ui.js
@@ -9,32 +9,11 @@ document.createElement("footer");
 document.createElement("main");
 
 $(function () {
- 
-  
+
 });
 
-
-
-
-
-
-
-// IE 하위 브라우저에 .ie_old 클래스 추가 => 프로젝트에 따라 변경될 수 있습니다.
-getInternetExplorerVersion();
-function getInternetExplorerVersion() {
-  var rv = -1; // Return value assumes failure.
-  if (navigator.appName != "Microsoft Internet Explorer"){
-    return;
-  }
-  else {
-    var ua = navigator.userAgent;
-    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-    if (re.exec(ua) != null)
-      rv = parseFloat(RegExp.$1);
-  }
-  if(rv < 8){ // IE브라우저 8버전 이하 시 ie_old 추가
-    rv = 8;
-    $("html").addClass("ie_old");
-  }
-  $("html").addClass("ie"+rv);
+function fnTab(obj,tabName){
+  var _this = obj;
+  $(_this).addClass('on').siblings('button').removeClass('on');
+  $('.tab_cont[data-tab="'+tabName+'"]').addClass('active').siblings('.tab_cont').removeClass('active');
 }
