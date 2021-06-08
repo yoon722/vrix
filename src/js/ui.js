f54cc497a6aa6da4d23640383add04aec76a9ae4
yoonison722 <yoonison722@gomcorp.com>
2021-06-08 16:35:41 +0900

    수정 - 3

--- src/js/ui.js
+++ src/js/ui.js
@@ -13,30 +13,23 @@ $(function () {
   //gnb
   var _gnb = $('.gnb');
   _gnb.find('>li>a').on('mouseenter focus',function(){
-      _gnb.find('li.on').removeClass('on').children('.sub_gnb').hide();
-      $(this).next().show().parent().addClass('on');
+    _gnb.find('li.on').removeClass('on').children('.sub_gnb').hide();
+    $(this).next().show().parent().addClass('on');
   });
   _gnb.find('.sub_gnb').on('mouseleave',function(){
-      $(this).parent('li.on').removeClass('on');
-      $(this).hide();
+    $(this).parent('li.on').removeClass('on');
+    $(this).hide();
   });
 
-  $('.tab_btns li').click(function(){
-    var idx = $(this).index();
-    $(this).removeClass("on");
-		$(this).eq(idx).addClass("on");
-    // $('tab_cont').removeClass('active');
-    // $('tab_cont').eq(idx).addClass('active');
-  });
 });
 
-// function fnTab(obj,tabName){
-//   var _this = obj;
-//   $(_this).addClass('on').siblings('button').removeClass('on');
-//   $('.tab_cont[data-tab="'+tabName+'"]').addClass('active').siblings('.tab_cont').removeClass('active');
-// }
+function fnTab(obj,tabName){ //beNX 참고 (by 정연과장님)
+  var _this = obj;
+  $(_this).addClass('on').siblings('button').removeClass('on');
+  $('.tab_cont[data-tab="'+tabName+'"]').addClass('active').siblings('.tab_cont').removeClass('active');
+}
 
 function closePopup() {
-	const popup = document.querySelector('.popup');
+	var popup = document.querySelector('.popup');
   popup.classList.add('hide');
 }
